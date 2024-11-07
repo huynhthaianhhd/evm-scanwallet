90aed6fccb7e3c8b4e8b523a9c90da427d7c579b
huynhthaianhhd <huynhthaianhhd@gmail.com>
2024-11-07 16:33:17 +0700

    up

--- modules/web3Factory.js
+++ modules/web3Factory.js
@@ -6,12 +6,7 @@ const hdkey = require('ethereumjs-wallet')
 class Web3Factory {
   constructor(_provider, _blockchain) {
     if (!_provider) throw new Error('Invalid provider')
-    this.web3 = new Web3(
-      new Web3.providers.HttpProvider(_provider, {
-        timeout: 30000, // Increase the timeout to 30 seconds
-        keepAlive: true, // Keep the connection open for reuse
-      })
-    )
+    this.web3 = new Web3(_provider)
     this.blockchain = _blockchain
     this.path = "m/44'/60'/0'/0/0"
   }
